6b3221dc20c03e1b77fc8b24c6f752e5414240f6
YonaGonca <ygoncalves5a@gmail.com>
2025-05-30 16:33:52 +0100

    project initialization with vite (React + tailwindCSS)

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <body className="bg-black text-white">
+      <div>
+        <h1 className="w-xl text-center my-0 mx-auto text-3xl font-bold text-blue-400">Tetris</h1>
+      </div>
+    </body>
+    </>
+  )
+}
+
+export default App
